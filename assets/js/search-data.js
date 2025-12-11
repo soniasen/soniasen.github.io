8f17807945cc59828da32a0c092083a16840b1d2
prashalibansal <prashali.90@gmail.com>
2025-12-11 06:42:18 +0000

    Deploying to gh-pages from @ soniasen/soniasen.github.io@32d53e4bdfc073e1e156e1cdb7d751f7dae4427d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "nav-github",
-          title: "GitHub",
-          description: "This page is under development",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-join-us",
           title: "Join Us",
           description: "",
